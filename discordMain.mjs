17823d2d8e70b2b236f88097df360b23b3b95654
BatteryB <130419015+BatteryB@users.noreply.github.com>
2024-02-09 02:29:53 +0900

    2024.02.09

--- discordMain.mjs
+++ discordMain.mjs
@@ -14,34 +14,50 @@ client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
     if (interaction.commandName === '출석') {
-        let currentDate = new Date();
-        let attendNum;
-        let year = currentDate.getFullYear(), month = currentDate.getMonth() + 1, day = currentDate.getDate(), id = interaction.user.id;
-        let join = await joinCheck(interaction.user.id), attendance = await attendanceCheck(interaction.user.id);
-
-        if (join == true && attendance == false) {
-
-            await new Promise((resolve, reject) => {
-                db.run('insert into attendance (year, month, day, id) values (?, ?, ?, ?)', [year, month, day, id], (err) => {
-                    if (err) reject(err);
-                    else resolve();
+        try {
+            let currentDate = new Date();
+            let year = currentDate.getFullYear(), month = currentDate.getMonth() + 1, day = currentDate.getDate(), id = interaction.user.id;
+            let attendNum;
+            let join = await joinCheck(interaction.user.id);
+            
+    
+            if (join) {
+                await new Promise((resolve, reject) => {
+                    db.run('DELETE FROM attendance WHERE id = ? AND year = ? AND month = ? AND day < ?', [id, year, month, day], (err) => {
+                        if (err) reject(err);
+                        else resolve();
+                    });
                 });
-            });
-
-            await new Promise((resolve, reject) => {
-                db.run('update user set attend = attend + 1, money = money + 10000 where id = ?', [id], (err) => {
-                    if (err) reject(err);
-                    else resolve();
-                });
-            });
-
-            attendNum = await attendanceUser(id);
-
-            await interaction.reply(attendNum.nickName + '님이 출석했습니다. 출석보너스 돈 +10000원\n누적 출석 횟수 : ' + attendNum.attend);
-        } else if (join == false) {
-            await interaction.reply({ content: '먼저 가입을 해주세요.', ephemeral: true });
-        } else {
-            await interaction.reply({ content: '이미 출석 하셨습니다.', ephemeral: true });
+                
+                let attendance = await attendanceCheck(interaction.user.id);
+    
+                if (!attendance) {
+                    await new Promise((resolve, reject) => {
+                        db.run('insert into attendance (year, month, day, id) values (?, ?, ?, ?)', [year, month, day, id], (err) => {
+                            if (err) reject(err);
+                            else resolve();
+                        });
+                    });
+    
+                    await new Promise((resolve, reject) => {
+                        db.run('update user set attend = attend + 1, money = money + 10000 where id = ?', [id], (err) => {
+                            if (err) reject(err);
+                            else resolve();
+                        });
+                    });
+    
+                    attendNum = await attendanceUser(id);
+    
+                    await interaction.reply(attendNum.nickName + '님이 출석했습니다. 출석보너스 돈 +10000원\n누적 출석 횟수 : ' + attendNum.attend);
+                } else {
+                    await interaction.reply({ content: '이미 출석 하셨습니다.', ephemeral: true });
+                }
+            } else {
+                await interaction.reply({ content: '먼저 가입을 해주세요.', ephemeral: true });
+            }
+        } catch (error) {
+            console.error('Error occurred:', error);
+            await interaction.reply({ content: '오류가 발생했습니다. 나중에 다시 시도해주세요.', ephemeral: true });
         }
     }
 
